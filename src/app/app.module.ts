059f2254c5381d8abf63a75bee4512969d184a13
enes-yilmaz02 <ey684241@gmail.com>
2023-02-02 20:34:50 +0300

    badge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,14 @@ import { AutoCompleteComponent } from './components/auto-complete/auto-complete.
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { BadgeComponent } from './components/badge/badge.component';
+import {MatBadgeModule} from '@angular/material/badge';
+
 @NgModule({
   declarations: [
     AppComponent,
-    AutoCompleteComponent
+    AutoCompleteComponent,
+    BadgeComponent
   ],
   imports: [
     BrowserModule,
@@ -19,7 +23,8 @@ import { FormsModule,ReactiveFormsModule } from '@angular/forms';
     MatAutocompleteModule,
     MatFormFieldModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatBadgeModule
    
   ],
   providers: [],
